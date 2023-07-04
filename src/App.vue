795b072bf85e1adf5978b895a05db6560eaaf37c
Andrea Creazzi <andreacreazzi@libero.it>
2023-07-04 15:34:05 +0200

    add SelectFilter (complete)

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import axios from 'axios';
 import { store } from './data/store';
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import SelectFilter from './components/SelectFilter.vue';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
 export default {
-  components: { AppHeader, AppMain },
+  components: { AppHeader, AppMain, SelectFilter },
   data() {
     return {
-      store
+      store,
+      filteredType: "",
     }
   },
   methods: {
@@ -16,6 +18,11 @@ export default {
       axios.get(url).then(res => {
         store.pokemons.item = res.data.docs
       })
+    },
+    pokemonType(option) {
+      this.filteredType = option
+      const filteredEndpoint = `${endpoint}?eq[type1]=${this.filteredType}`
+      this.fetchPokemons(filteredEndpoint)
     }
   },
   created() {
@@ -25,6 +32,7 @@ export default {
 </script>
 <template>
   <AppHeader />
+  <SelectFilter @option-change="pokemonType" />
   <AppMain />
 </template>
 <style></style>
--- src/components/SelectFilter.vue
+++ src/components/SelectFilter.vue
@@ -1,11 +1,46 @@
 <script>
 export default {
     data() {
-        return {}
-    }
+        return {
+            selectType: "",
+            listTypes: [
+                "Bug",
+                "Dark",
+                "Dragon",
+                "Electric",
+                "Fairy",
+                "Fighting",
+                "Fire",
+                "Flying",
+                "Ghost",
+                "Grass",
+                "Ground",
+                "Ice",
+                "Normal",
+                "Poison",
+                "Psychic",
+                "Rock",
+                "Steel",
+                "Water"
+            ]
+        }
+    },
+    emits: ['option-change', 'select-submit']
 }
 </script>
 
-<template></template>
+<template>
+    <div class="container mb-5">
+        <select @change="$emit('option-change', selectType)" v-model="selectType" id="inputState"
+            class="form-select text-center" placeholder="choose okemon type">
+            <option selected v-for="listType of listTypes">{{ listType }}
+            </option>
+        </select>
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+select {
+    border-color: red;
+}
+</style>
